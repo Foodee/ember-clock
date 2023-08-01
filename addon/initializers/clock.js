df14f5f7afd01bca431589de5267d1aea91bceeb
Joe Gaudet <joe@joegaudet.com>
2023-08-01 16:13:17 -0700

    Update clock.js

--- addon/initializers/clock.js
+++ addon/initializers/clock.js
@@ -2,9 +2,7 @@
   Inject the clock service into all controllers and components
 */
 export function initialize() {
-  const app = arguments[1] || arguments[0];
-  app.inject('controller', 'clock', 'service:clock');
-  app.inject('component', 'clock', 'service:clock');
+
 }
 
 export default {
